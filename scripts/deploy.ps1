7281f39e480bb18c0a2535bd0a0cce54a11361ce
Kasim Ahmic <kasim.ahmic@gmail.com>
2026-09-21 02:11:08 -0400

    chore: Add deploy utility script

---
+++ scripts/deploy.ps1
@@ -0,0 +1,5 @@
+﻿dotnet build -c Release
+
+scp `
+    TaskManager\bin\Release\netstandard2.1\TaskManager.dll `
+    deck@steamdeck:"/home/deck/.steam/steam/steamapps/common/TCG Card Shop Simulator/BepInEx/plugins/TaskManager.dll"
